46837345bcf10b9add6ad83495e5146d82eae096
ArturGacek <arthur.gacek@gmail.com>
2020-04-22 20:11:34 +0200

    toolbar/side drawer

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import classes from './Toolbar.module.css'
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+
+const Toolbar = (props) => {
+    return (
+        <header className={classes.Toolbar}>
+            <div>MENU</div>
+            <div className={classes.Logo}>
+                <Logo/>
+            </div>
+            {/*<Logo height="80%"/>*/}
+            <nav>
+                <NavigationItems/>
+            </nav>
+        </header>
+    );
+};
+
+export default Toolbar;
