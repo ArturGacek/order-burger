8e953ff6383dc906afd405dbecad949bf753a670
ArturGacek <arthur.gacek@gmail.com>
2020-05-16 22:14:04 +0200

    implement authentication basics

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,7 +6,8 @@ const NavigationItems = () => {
     return (
         <ul className={classes.NavigationItems}>
             <NavigationItem link="/" exact > Burger Builder </NavigationItem>
-            <NavigationItem link="/Orders"> Checkout </NavigationItem>
+            <NavigationItem link="/Orders"> Orders </NavigationItem>
+            <NavigationItem link="/Auth"> Authenticate </NavigationItem>
         </ul>
     );
 };
