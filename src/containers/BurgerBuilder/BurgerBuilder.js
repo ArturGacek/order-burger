534f9d50b6087833ff2a0d1deec63a14bc4d5b97
ArturGacek <arthur.gacek@gmail.com>
2020-05-21 09:52:24 +0200

    add few simple tests

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -19,7 +19,7 @@ import * as actions from '../../store/actions/index'
 // };
 
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
     // constructor(props) {
     //     super(props);
     //     this.state = {...}
---
+++ src/store/reducers/auth.test.js
@@ -0,0 +1,35 @@
+import * as actionTipes from '../actions/actionTypes';
+import reducer from "./auth";;
+
+
+describe('auth reducer', () => {
+    it('sholud return the initial state', () => {
+        expect(reducer(undefined, {})).toEqual({
+            token: null,
+            userId: null,
+            error: null,
+            loading: false,
+            authRedirectPath: '/'
+        })
+    });
+
+    it('should store the token upon login', () => {
+        expect(reducer({
+            token: null,
+            userId: null,
+            error: null,
+            loading: false,
+            authRedirectPath: '/'
+        }, {
+            type: actionTipes.AUTH_SUCCESS,
+            idToken: 'some-token',
+            userId: 'some-user-id'
+        })).toEqual({
+            token: 'some-token',
+            userId: 'some-user-id',
+            error: null,
+            loading: false,
+            authRedirectPath: '/'
+        })
+    });
+});
